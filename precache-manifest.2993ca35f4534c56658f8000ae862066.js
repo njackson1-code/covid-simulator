adefe516c14a5b9232b302c22313da1a263ceca1
Noah Jackson <noahjackson@Noahs-MacBook-Air.local>
2020-07-17 15:39:13 -0400

    Updates

---
+++ precache-manifest.2993ca35f4534c56658f8000ae862066.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c961ddfc796f6eb7b4bf16b0193144",
+    "url": "/covid-simulator/index.html"
+  },
+  {
+    "revision": "342ffc4ae9d4c0619459",
+    "url": "/covid-simulator/static/css/main.8d9e63bf.chunk.css"
+  },
+  {
+    "revision": "5e413e89964fabbeccbb",
+    "url": "/covid-simulator/static/js/2.4b5a8561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-simulator/static/js/2.4b5a8561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342ffc4ae9d4c0619459",
+    "url": "/covid-simulator/static/js/main.1ab56882.chunk.js"
+  },
+  {
+    "revision": "cdf0e76644ad533e12d5",
+    "url": "/covid-simulator/static/js/runtime-main.8af1a212.js"
+  },
+  {
+    "revision": "a625a7c2b1a47866209067fe30f99368",
+    "url": "/covid-simulator/static/media/coronavirus.a625a7c2.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-simulator/static/media/logo.5d5d9eef.svg"
+  }
+]);
